9049ce6aaa02d4ecfa44add098ab53985518ff05
astafan8 <astafan8@gmail.com>
2018-07-03 15:43:40 +0200

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='v0_characterization',
+    version='0.0.1',
+
+    install_requires=['qcodes'],
+
+    author='Mikhail Astafev',
+    author_email='astafan8@gmail.com',
+
+    description=("Package with code and scripts for performing V0 "
+                 "characterization measurements in Delft."),
+
+    license='',
+
+    packages=['v0_utils'],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.6'
+        ],
+
+    keywords='v0 characterization',
+
+    url='https://github.com/kouwenhovenlab/v0_characterization'
+)
