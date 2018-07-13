22a8054254bde8eef3af48d5d2ea6a9c7fa73ff5
Wolfgang Pfaff <wopfa@microsoft.com>
2018-07-13 17:39:45 +0200

    Added sequencer and sequence generator for single channel - 2 marker situation (one rising, one falling)

--- v0_utils/awg_sequencers.py
+++ v0_utils/awg_sequencers.py
@@ -13,7 +13,8 @@ from qcodes.utils.validators import Numbers
 
 from .qcodes_tools import VirtualInstrument
 from .sequence_generators import SequenceGenerator, \
-    OneChannelOneMarkerGenerator
+    OneChannelOneMarkerGenerator, \
+    OneChannelTwoMarkerGenerator
 
 
 class AwgSequencer(VirtualInstrument):
@@ -97,7 +98,7 @@ class OneChannelOneMarkerAwgSequencer(AwgSequencer):
                            get_cmd=self._get_marker_high,
                            set_cmd=self._set_marker_high,
                            get_parser=float,
-                           initial_value=1.75,
+                           initial_value=1.4,
                            vals=Numbers(),
                            docstring="The marker high value for the marker "
                                      "channel within the analog channel."
@@ -207,3 +208,201 @@ class OneChannelOneMarkerAwgSequencer(AwgSequencer):
         """
         self._send_sequence_to_awg_and_load()
         self._setup_awg_channels_for_sequence()
+
+        
+class OneChannelTwoMarkerAwgSequencer(AwgSequencer):
+    """
+    An AWG sequence virtual instrument that uploads broadbean sequences
+    that contain only one analog channel and two marker channels (that is
+    attached to it).
+    """
+
+    def __init__(self,
+                 name: str,
+                 awg_obj: Type[AWG70000A],
+                 sequence_gen_obj: Type[OneChannelTwoMarkerGenerator],
+                 **kwargs):
+        super().__init__(name, awg_obj, sequence_gen_obj, **kwargs)
+
+        # TODO: AGAIN, WHAT IS THIS???
+        self.add_parameter(name='channel_p_p_amplitude_factor',
+                           label='Channel peak-to-peak amplitude at AWG',
+                           unit='V',  # TODO: is it correct?
+                           get_cmd=None,
+                           set_cmd=None,
+                           get_parser=float,
+                           initial_value=1.0,
+                           vals=Numbers(),
+                           docstring="It is that '2/f' thing for converting "
+                                     "broadbean a.u. into voltage."
+                                     "Value of '1' means that '1' a.u. in "
+                                     "broadbean sequence == '1 V' in AWG."
+                           )
+        # TODO: AGAIN, WHAT IS THIS???
+        self.add_parameter(name='channel_amplitude_forging',
+                           label='Channel amplitude for forging sequence',
+                           unit='V',  # TODO: is it correct?
+                           get_cmd=None,
+                           set_cmd=None,
+                           get_parser=float,
+                           initial_value=1.0,
+                           vals=Numbers(),
+                           docstring="In general case, this should be a list "
+                                     "of aplitudes for each used channel in "
+                                     "the broadbean sequence."
+                           )
+        self.add_parameter(name='marker_falling_channel_high',
+                           label='Marker falling channel high',
+                           unit='V',
+                           get_cmd=self._get_marker_falling_high,
+                           set_cmd=self._set_marker_falling_high,
+                           get_parser=float,
+                           initial_value=1.4,
+                           vals=Numbers(),
+                           docstring="The marker high value for the marker "
+                                     "channel for the falling edge within the analog channel."
+                           )
+        self.add_parameter(name='marker_rising_channel_high',
+                           label='Marker rising channel high',
+                           unit='V',
+                           get_cmd=self._get_marker_rising_high,
+                           set_cmd=self._set_marker_rising_high,
+                           get_parser=float,
+                           initial_value=2.5,
+                           vals=Numbers(),
+                           docstring="The marker high value for the marker "
+                                     "channel for the rising edge within the analog channel."
+                           )
+        self.add_parameter(name='marker_channel_low',
+                           label='Marker channel low',
+                           unit='V',
+                           get_cmd=self._get_marker_low,
+                           set_cmd=self._set_marker_low,
+                           get_parser=float,
+                           initial_value=0.0,
+                           vals=Numbers(),
+                           docstring="The marker low value for the marker "
+                                     "channel within the analog channel."
+                           )
+
+    def _get_channel_resolution(self):
+        return max(awg_channel_resolutions[self.awg.model])
+
+    def _get_channel_obj(self):
+        return self.awg.channels[self.sequence_gen.channel_number() - 1]
+
+    def _get_parameter_obj_of_marker_falling(self, subname: str) -> Parameter:
+        """
+        ...
+
+        Args:
+            subname
+                stuff like "high", "low", etc (in order to generate the name
+                of a callable like "marker{marker_channel_number}_{subname}"
+        """
+        return getattr(
+            self._get_channel_obj(),
+            f"marker{self.sequence_gen.marker_falling_channel_number()}_{subname}"
+        )
+
+    def _get_marker_falling_high(self):
+        return self._get_parameter_obj_of_marker_falling("high")()
+
+    def _set_marker_falling_high(self, marker_high):
+        self._get_parameter_obj_of_marker_falling("high")(marker_high)
+
+    def _get_marker_falling_low(self):
+        return self._get_parameter_obj_of_marker_falling("low")()
+
+    def _set_marker_falling_low(self, marker_low):
+        self._get_parameter_obj_of_marker_falling("low")(marker_low)
+        
+    def _get_parameter_obj_of_marker_rising(self, subname: str) -> Parameter:
+        """
+        ...
+
+        Args:
+            subname
+                stuff like "high", "low", etc (in order to generate the name
+                of a callable like "marker{marker_channel_number}_{subname}"
+        """
+        return getattr(
+            self._get_channel_obj(),
+            f"marker{self.sequence_gen.marker_rising_channel_number()}_{subname}"
+        )
+
+    def _get_marker_rising_high(self):
+        return self._get_parameter_obj_of_marker_rising("high")()
+
+    def _set_marker_rising_high(self, marker_high):
+        self._get_parameter_obj_of_marker_rising("high")(marker_high)
+
+    def _get_marker_rising_low(self):
+        return self._get_parameter_obj_of_marker_rising("low")()
+
+    def _set_marker_rising_low(self, marker_low):
+        self._get_parameter_obj_of_marker_rising("low")(marker_low)
+        
+    def _get_forged_sequence(self):
+        sequence_obj = self.sequence_gen.make_broadbean_sequence()
+        forged_sequence = sequence_obj.forge()
+        return forged_sequence
+
+    def _send_sequence_to_awg_and_load(self) -> None:
+        """
+        .....
+        """
+        # forge the sequence
+        forged_sequence = self._get_forged_sequence()
+
+        # create a sequence file
+        seqx_file = self.awg.make_SEQX_from_forged_sequence(
+            forged_sequence,
+            np.atleast_1d(self.channel_amplitude_forging()),
+            self.sequence_gen.sequence_name)
+        seqx_file_name = f'{self.sequence_gen.sequence_name}.seqx'
+
+        # clear lists of sequences and waveforms on the instrument in order
+        # to prevent cluttering
+        self.awg.stop()
+        self.awg.clearSequenceList()
+        self.awg.clearWaveformList()
+
+        # send the sequence file to the instrument and load it
+        self.awg.sendSEQXFile(seqx_file, filename=seqx_file_name)
+        self.awg.loadSEQXFile(seqx_file_name)
+
+    def _setup_awg_channels_for_sequence(self) -> None:
+        """
+        ..........
+        """
+        self.awg.sample_rate(self.sequence_gen.sample_rate())
+
+        channel_obj = self._get_channel_obj()
+
+        # Add sequence to the channel
+        track_number = 1
+        channel_obj.setSequenceTrack(self.sequence_gen.sequence_name,
+                                     track_number)
+
+        channel_obj.awg_amplitude(self.channel_p_p_amplitude_factor())
+
+        # We reserve 14 bits for the analog channel, so that we have 2 bits for
+        # the markers 1 and 2
+        channel_obj.resolution(
+            self._get_channel_resolution() - self.sequence_gen.number_of_markers)
+
+        channel_obj.state(1)  # means "on"
+
+    def send_sequence_and_setup_awg(self) -> None:
+        """
+        This performs the physical action of generation the sequence from
+        the sequence generator, forging it, sending it as a sequence file
+        to AWG, loading it, and assigning to channel.
+
+        It is very useful to add this as a "before_run" for "Measurement"
+        """
+        self._send_sequence_to_awg_and_load()
+        self._setup_awg_channels_for_sequence()
+
+        
--- v0_utils/sequence_generators.py
+++ v0_utils/sequence_generators.py
@@ -30,6 +30,13 @@ class OneChannelOneMarkerGenerator(SequenceGenerator):
     number_of_markers = 1
     number_of_channels = 1
 
+    
+class OneChannelTwoMarkerGenerator(SequenceGenerator):
+    # TODO: make these generic, preferably extractable from broadbean sequence object or smth
+    number_of_markers = 2
+    number_of_channels = 1
+    
+
 
 class RepeatingStaircaseRampGenerator(OneChannelOneMarkerGenerator):
     """
@@ -195,6 +202,212 @@ class RepeatingStaircaseRampGenerator(OneChannelOneMarkerGenerator):
                 step_segment_name,
                 (0, self.settlement_time()),
                 self.marker_channel_number()
+            )
+
+        # Create an element out of the staircase blueprint
+
+        staircase_element = broadbean.Element()
+        staircase_element.addBluePrint(
+            self.channel_number(), staircase_blueprint)
+
+        # Create a sequence
+
+        sequence = broadbean.Sequence()
+        sequence.setSR(self.sample_rate())
+
+        # Add the fast staircase element to the sequence
+
+        waveform_id = 1
+        sequence.addElement(waveform_id, staircase_element)
+        sequence.setSequencingTriggerWait(waveform_id, 0)
+        sequence.setSequencingNumberOfRepetitions(waveform_id,
+                                                  self.n_repetitions())
+        # # infinite sequence - useful for debugging on hardware
+        # sequence.setSequencingGoto(waveform_id, goto=waveform_id)
+
+        return sequence
+        
+        
+class RepeatingStaircaseRampGeneratorTwoTriggers(OneChannelTwoMarkerGenerator):
+    """
+    Generates broadbean sequence for a staircase with two marker signals for
+    triggering.
+    """
+
+    def __init__(self,
+                 name: str,
+                 **kwargs):
+        super().__init__(name, **kwargs)
+
+        # Base this class on the repeating staircase ramp object
+        # ("inclusion" as opposed to "subclassing" is used in order to
+        # decouple the implementation details of the staircase ramp object)
+        self.add_submodule('staircase_ramp',
+                           RepeatingStaircaseRamp('repeating_staircase_ramp')
+                           )
+        self.delegate_attr_objects.append('staircase_ramp')
+
+        # Channel definitions
+        # TODO: somewhere MAX VALUE has to meet the AWG channel number limit
+        self.add_parameter(name='channel_number',
+                           label='Signal channel number',
+                           unit='',
+                           get_cmd=None,
+                           set_cmd=None,
+                           get_parser=int,
+                           initial_value=1,
+                           vals=Numbers(min_value=1),
+                           docstring="Number of the channel for the analog "
+                                     "signal"
+                           )
+        # TODO: somewhere MAX VALUE has to meet the AWG marker channel number limit
+        self.add_parameter(name='marker_falling_channel_number',
+                           label='Marker falling channel number',
+                           unit='',
+                           get_cmd=None,
+                           set_cmd=None,
+                           get_parser=int,
+                           initial_value=1,
+                           vals=Numbers(min_value=1),
+                           docstring="Number of the channel for the marker "
+                                     "signal for the falling edge within the analog channel"
+                           )
+        self.add_parameter(name='marker_rising_channel_number',
+                           label='Marker rising channel number',
+                           unit='',
+                           get_cmd=None,
+                           set_cmd=None,
+                           get_parser=int,
+                           initial_value=2,
+                           vals=Numbers(min_value=1),
+                           docstring="Number of the channel for the marker "
+                                     "signal for the rising edge within the analog channel"
+                           )
+
+        # Waveform generation specific parameters
+        self.add_parameter(name='sample_rate',
+                           # TODO: perhaps should be derived from AWG settings and limits
+                           label='Sample rate',
+                           unit='S/s',
+                           get_cmd=None,
+                           set_cmd=None,
+                           get_parser=float,
+                           initial_value=10e3,
+                           vals=Numbers(min_value=0),
+                           docstring="sample rate for AWG - we need it upfront for defining the broadbean sequence"
+                           )
+        self.add_parameter(name='settlement_time',
+                           label='Time within step before trigger signal',
+                           unit='s',
+                           get_cmd=None,
+                           set_cmd=None,
+                           get_parser=float,
+                           initial_value=1e-3,
+                           vals=Numbers(min_value=0),
+                           docstring="after this much time, the marker's "
+                                     "falling edge occurs, hence the sample "
+                                     "is acquired on hte lock-in this value depends on the system-under-measurement"
+                           )
+        self.add_parameter(name='lockin_integration_time',
+                           # TODO: perhaps rename to smth lockin-independent
+                           label='Time within step after trigger signal',
+                           unit='s',
+                           get_cmd=None,
+                           set_cmd=None,
+                           get_parser=float,
+                           initial_value=1e-3,
+                           vals=Numbers(min_value=0),
+                           docstring="this much time WE THINK is required by "
+                                     "the lockin for correctly acquiring one sample"
+                           )
+        self.add_parameter(name='restart_compensation',
+                           label='Delay before the start of staircase',
+                           unit='s',
+                           get_cmd=None,
+                           set_cmd=None,
+                           get_parser=float,
+                           initial_value=0,
+                           vals=Numbers(min_value=0),
+                           docstring="The very first step is made a bit longer "
+                                     "in order to accommodate settlement "
+                                     "required for the system when the "
+                                     "staircase is restarted (i.e. when the "
+                                     "jump is much bigger than the jump "
+                                     "between individual steps)"
+                           )
+
+        # Convenient get-only parameters
+        self.add_parameter(name='step_duration',
+                           label='Step duration',
+                           unit='#',
+                           get_cmd=self._get_step_duration,
+                           set_cmd=False,
+                           get_parser=float,
+                           vals=Numbers(),
+                           docstring="Duration of a single step of the "
+                                     "staircase"
+                           )
+
+    def _get_step_duration(self):
+        return self.settlement_time() + self.lockin_integration_time()
+
+    def get_trigger_moments_vector(self):
+        return np.arange(
+            self.settlement_time(),  # from
+            self.n_all_steps() * self.step_duration(),  # until
+            self.step_duration()  # distance between points
+        )
+
+    def make_broadbean_sequence(self) -> broadbean.Sequence:
+        """
+        Generates a broadbean sequence according to the set parameters.
+
+        :return: Broadbean sequence object
+        """
+        # Create blueprint
+        # this thing is called "waveform" within AWG
+        staircase_blueprint = broadbean.BluePrint()
+        staircase_blueprint.setSR(self.sample_rate())
+
+        ramp = broadbean.PulseAtoms.ramp
+
+        # Compensation for the restart of the staircase
+        if self.restart_compensation() > 0:
+            staircase_blueprint.insertSegment(
+                -1,  # append to the end of the blueprint
+                ramp,
+                (self.start_ramp_voltage(), self.start_ramp_voltage()),
+                # plateau
+                dur=self.restart_compensation(),
+                name="staircase_restart_compensation"
+            )
+
+        for step_number, step_voltage in enumerate(
+                np.linspace(self.start_ramp_voltage(),
+                            self.finish_ramp_voltage(),
+                            self.n_steps())):
+            step_segment_name = f"fast_step_{step_number}_"
+
+            # the analog plateau signal
+            staircase_blueprint.insertSegment(
+                -1,  # append to the end of the blueprint
+                ramp, (step_voltage, step_voltage),  # flat plateau
+                dur=self.step_duration(),
+                name=step_segment_name
+            )
+
+            # the digital trigger signal with a falling edge
+            staircase_blueprint.setSegmentMarker(
+                step_segment_name,
+                (0, self.settlement_time()),
+                self.marker_falling_channel_number()
+            )
+            
+            # the digital trigger signal with a rising edge
+            staircase_blueprint.setSegmentMarker(
+                step_segment_name,
+                (self.settlement_time(), self.lockin_integration_time()),
+                self.marker_rising_channel_number()
             )
 
         # Create an element out of the staircase blueprint
